33e89aae932c6e188a762d395ef5a1aa3169b871
Yewantraj Karki <54407001+yewant@users.noreply.github.com>
2024-09-10 04:39:48 +0000

    changed about image and updated some css to adjust height

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-about.jpg'
+import ME from '../../assets/aboutme.jpg'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {VscFolderLibrary} from 'react-icons/vsc'
@@ -24,19 +24,19 @@ const About = () => {
         <article className='about__card'>
           <FaAward className='about__icon'/>
           <h5>Experience</h5>
-          <small>3+ Years Working</small>
+          <small>n/a</small>
         </article>
 
         <article className='about__card'>
           <FiUsers className='about__icon'/>
           <h5>Clients</h5>
-          <small>200+ Worldwide</small>
+          <small>n/a</small>
         </article>
 
         <article className='about__card'>
           <VscFolderLibrary className='about__icon'/>
           <h5>Projects</h5>
-          <small>40+ Completed</small>
+          <small>n/a</small>
         </article>
         </div>
 
